18df0cfe39a066766717a9161e15b4d3d4f23554
spriya14 <surabhi.priya2514@gmail.com>
2025-05-30 16:40:16 -0700

    adding my first commit in artsy

---
+++ my-website/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Hey There! Wasssup! <br/>
+          Thank You for stopping by.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
