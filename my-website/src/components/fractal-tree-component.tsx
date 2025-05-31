c124cddcc56a931095d98da68fe9a452e8971cc4
spriya14 <surabhi.priya2514@gmail.com>
2025-05-30 18:26:14 -0700

    testing pages changes

--- my-website/src/components/fractal-tree-component.tsx
+++ my-website/src/components/fractal-tree-component.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 
-const FractalTree: React.FC =() => {
+
+const FractalTree: React.FC = () => {
     return (
         <div className="fractal-tree">
-            <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" stroke="black" strokeWidth="0.5">
                     <line x1="50" y1="100" x2="50" y2="80" />
                     <line x1="50" y1="80" x2="40" y2="70" />
